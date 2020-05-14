4e1ee687675d5a47344d294b95573b1109a01ef3
lucasmallmann <lucasmallmann76@gmail.com>
2020-05-14 00:02:02 -0300

    styled approach

--- pages/second.js
+++ pages/second.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import AwesomeSlider from "react-awesome-slider";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
+import media from "styled-media-query";
 
 const Description = styled.div`
   display: flex;
 
-  flex-direction: ${(props) => (props.mobile ? "column" : "row")};
+  ${media.lessThan("500px")`
+    flex-direction: column;
+  `}
 
   margin: 0 auto;
   box-sizing: border-box;
@@ -23,60 +26,64 @@ const Description = styled.div`
   }
 `;
 
-const GroupDescription = ({ mobile }) => (
-  <>
-    <Description mobile={mobile}>
-      <Box mobile={mobile} />
-      <p>
-        Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
-        ipsumLorem ipsum
-      </p>
-    </Description>
+const DescriptionContainer = styled.div`
+  ${media.lessThan("500px")`
+    display: none;
+  `}
+`;
 
-    <Description mobile={mobile}>
-      <Box mobile={mobile} />
-      <p>
-        Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
-        ipsumLorem ipsum
-      </p>
-    </Description>
-  </>
-);
+const SliderContainer = styled.div`
+  ${media.lessThan("500px")`
+    display: block;
+  `}
 
-const MOBILE_WIDTH = 480;
+  ${media.greaterThan("500px")`
+    display: none;
+  `}
+`;
 
 export default function Second() {
-  const [mobile, setMobile] = useState(false);
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const width = window.innerWidth;
-      setMobile(width < MOBILE_WIDTH);
-    }
-
-    window.addEventListener("resize", () => {
-      const width = window.innerWidth;
-      setMobile(width < MOBILE_WIDTH);
-    });
-
-    return () => {
-      // Component will unmount
-      window.removeEventListener("resize", () => {});
-    };
-  }, []);
-
   return (
     <>
-      {!mobile ? (
-        <GroupDescription />
-      ) : (
-        <AwesomeSlider>
-          <GroupDescription />
-        </AwesomeSlider>
-      )}
-      <button onClick={() => setMobile(!mobile)}>Toogle mobile</button>
+      <DescriptionContainer>
+        <Description>
+          <Box />
+          <p>
+            Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
+            ipsumLorem ipsumLorem ipsum
+          </p>
+        </Description>
 
-      <span>{mobile.toString()}</span>
+        <Description>
+          <Box />
+          <p>
+            Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
+            ipsumLorem ipsumLorem ipsum
+          </p>
+        </Description>
+      </DescriptionContainer>
+
+      {/* Div que engloba o slider */}
+      <SliderContainer>
+        <AwesomeSlider>
+          <Description>
+            <Box />
+            <p>
+              Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
+              ipsumLorem ipsumLorem ipsum
+            </p>
+          </Description>
+
+          <Description>
+            <Box />
+            <p>
+              Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
+              ipsumLorem ipsumLorem ipsum
+            </p>
+          </Description>
+        </AwesomeSlider>
+      </SliderContainer>
+      <button onClick={() => setMobile(!mobile)}>Toogle mobile</button>
     </>
   );
 }
@@ -87,9 +94,7 @@ const Box = styled.div`
   border-radius: 50%;
   background-color: pink;
 
-  ${(props) =>
-    props.mobile &&
-    css`
-      margin: 0 auto;
-    `}
+  ${media.lessThan("500px")`
+    margin: 0 auto;
+  `}
 `;
